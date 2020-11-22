ec9ba0707d291ea29ee81a974c6789562c5f1748
Katia <katia.punter@storyshareplatform.com>
2020-11-22 17:56:17 +0000

    Rewrites first test better

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-});
+describe("Checks RTL works fine", () => {
+  test("App should render without crashing", () => {
+    render(<App />);
+  });
+})
