ce6ba15e694ee0582db78d9afd8e0d3ff886688c
Katia <katia.punter@storyshareplatform.com>
2020-11-22 17:58:11 +0000

    Adds to test one debug that shows html of app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("Checks RTL works fine", () => {
   test("App should render without crashing", () => {
     render(<App />);
+    screen.debug();
   });
 })
