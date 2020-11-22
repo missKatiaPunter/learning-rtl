d34629c396ca342f0f52751c3d080425f898487f
Katia <katia.punter@storyshareplatform.com>
2020-11-22 18:34:08 +0000

    Breaks the test by adding another word to div

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("Checks RTL works fine", () => {
-  test("App should render without crashing", () => {
+  test("App should render without crashing and should render text RTL", () => {
     render(<App />);
     screen.debug();
     screen.getByText("RTL");
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>RTL</div>
+    <div>Learning RTL</div>
   );
 }
 
