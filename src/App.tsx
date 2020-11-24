7fd35183ac4b4939e1b67bc8c63aee586049dcb6
Katia <katia.punter@storyshareplatform.com>
2020-11-24 08:14:20 +0000

    Huge challenge accomplished I added 2 nums in ts

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ function App() {
   return (
     <>
       <h1>Learning RTL</h1>
-      <CustomInput />
+      <CustomInput>
+        Input some text in this textbox
+      </CustomInput>
+      <p>You typed</p>
     </>
   );
 }
 
 const CustomInput: FunctionComponent = (props) => {
+  const { children } = props;
   return (
     <>
-      <input type="text" />
+      <label htmlFor="custom_input">{ children }</label>
+      <input id="custom_input" type="text" />
     </>
   );
 }
