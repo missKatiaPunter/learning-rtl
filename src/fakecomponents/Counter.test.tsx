9e056a5c180d587a2ee3854e95214d2fb9c11f8c
Katia <katia.punter@storyshareplatform.com>
2020-11-24 19:49:48 +0000

    Tests with fireEvent

--- src/fakecomponents/Counter.test.tsx
+++ src/fakecomponents/Counter.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import Counter from './Counter';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { Counter } from './Counter';
 
 test("the Counter component using a wrapper variable", () => {
     const wrapper = render(<Counter />);
@@ -9,10 +9,23 @@ test("the Counter component using a wrapper variable", () => {
     expect(wrapper.getByTestId('counter-button').tagName).toBe('BUTTON');
 });
 
+//Get element with getByTestId (data api)
 // You can use data attribute to select elements for testing with RTL
 
 test("the Counter component", () => {
     const { debug, getByTestId } = render(<Counter />);
     debug();
     expect(getByTestId('counter-button').tagName).toBe('BUTTON');
+    expect(getByTestId('counter-button').textContent).toBe('0');
+})
+
+// Using fireEvent
+
+test("the Counter component with fire event", () => {
+    const { getByTestId } = render(<Counter />);
+    const counterButton = getByTestId('counter-button');
+    fireEvent.click(counterButton);
+    expect(counterButton.textContent).toBe('1');
+    fireEvent.click(counterButton);
+    expect(counterButton.textContent).toBe('2');
 })
--- src/fakecomponents/Counter.tsx
+++ src/fakecomponents/Counter.tsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class Counter extends Component {
-    state = { counter: 0}
-    render() {
-        return (
-            <div>
-                <button data-testid="counter-button">{this.state.counter}</button>
-            </div>
-        )
-    }
+export const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    return (
+        <div>
+            <button
+                onClick={ () =>  setCounter(counter+1)}
+                data-testid="counter-button"
+            >
+                {counter}
+            </button>
+        </div>
+    )
 }
